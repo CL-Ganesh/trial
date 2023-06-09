f91ae99fd452837350aea226cfccc9be5f5a4c02
CL-Ganesh <133627079+CL-Ganesh@users.noreply.github.com>
2023-06-09 12:46:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Testing Pipeline new 5 ");
+  res.send("Testing Pipeline new 6 ");
 });
 
 app.get("/info", (req, res) => {
